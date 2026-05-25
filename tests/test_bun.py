805cb0a298efa5a607c44e4d55ac1b4c27b50e4c
Ilya <156908087+IlyaCh13@users.noreply.github.com>
2026-05-25 03:32:22 +0300

    Refactor tests: move fixtures to conftest, simplify test structure

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -1,23 +1,14 @@
 import pytest
-
 from praktikum.bun import Bun
 
 
-class TestBun:
-    # Параметризуем разными названиями булок, чтобы убедиться, что get_name
-    # возвращает именно то название, которое было передано при создании
-    @pytest.mark.parametrize("name", [
-        "black bun",
-        "white bun",
-        "red bun",
-        "sesame bun",
-    ])
-    def test_get_name_returns_correct_name(self, name):
-        bun = Bun(name, 100)
-        assert bun.get_name() == name
+@pytest.mark.parametrize('name', ['black bun', 'white bun', 'red bun'])
+def test_bun_get_name(name):
+    bun = Bun(name, 100)
+    assert bun.get_name() == name
 
-    # Параметризуем ценами, включая граничные значения (0, дробные, большие числа)
-    @pytest.mark.parametrize("price", [0, 100, 200.5, 999.99])
-    def test_get_price_returns_correct_price(self, price):
-        bun = Bun("test bun", price)
-        assert bun.get_price() == price
+
+@pytest.mark.parametrize('price', [0, 100, 200.5])
+def test_bun_get_price(price):
+    bun = Bun('test bun', price)
+    assert bun.get_price() == price
